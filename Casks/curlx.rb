8da886112023a32dcb599cae5e44c0bba8b0c0e9
cntrump <cntrump@gmail.com>
2020-12-06 07:52:36 +0800

    Update curlx.rb

--- Casks/curlx.rb
+++ Casks/curlx.rb
@@ -2,7 +2,7 @@ cask "curlx" do
   version "7.73.0"
   sha256 "96d3b2de00501375bd3e6468ce0bbd7ff5f235987f040227239ee9442489de6f"
 
-  url "https://github.com/cntrump/homebrew-curlx/releases/download/v#{version}/curlx-#{version}-macos.zip"
+  url "https://github.com/cntrump/homebrew-brew/releases/download/v#{version}/curlx-#{version}-macos.zip"
   name "curlx"
   desc "HTTP/3 version curl."
   homepage "https://github.com/cntrump/build_curl/"
