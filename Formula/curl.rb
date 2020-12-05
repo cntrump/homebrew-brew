731f1ffde961c8b3ec7d26e987a4de29d21a2bac
cntrump <cntrump@gmail.com>
2020-12-05 19:30:24 +0800

    Create curl.rb

---
+++ Formula/curl.rb
@@ -0,0 +1,10 @@
+class curl < Formula
+  desc "curl is used in command lines or scripts to transfer data."
+  homepage "https://github.com/cntrump/build_curl/"
+  url "https://github.com/cntrump/homebrew-brew/releases/download/v7.73.0/curl-7.73.0_macosx.zip"
+  sha256 "fa6534488b29d7a506f200863434c1e1487faa7eba1728f1ae2b786aaa15179d"
+
+  test do
+    system "#{bin}/curl --version"
+  end
+end
