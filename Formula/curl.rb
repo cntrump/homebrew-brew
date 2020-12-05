b864743c77c3700dae5f840a58a5eb2d5e60a0ff
cntrump <cntrump@gmail.com>
2020-12-05 19:34:25 +0800

    Update curl.rb

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -4,6 +4,10 @@ class Curl < Formula
   url "https://github.com/cntrump/homebrew-brew/releases/download/v7.73.0/curl-7.73.0_macosx.zip"
   sha256 "fa6534488b29d7a506f200863434c1e1487faa7eba1728f1ae2b786aaa15179d"
 
+  def install
+    bin.install "curl"
+  end
+  
   test do
     system "#{bin}/curl --version"
   end
