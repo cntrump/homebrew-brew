4b327d256074c5ff5152f09bad21a6620fd5925a
cntrump <cntrump@gmail.com>
2020-12-05 21:54:36 +0800

    Update curlx.rb

--- Formula/curlx.rb
+++ Formula/curlx.rb
@@ -1,7 +1,7 @@
 class Curlx < Formula
-  desc "curl is used in command lines or scripts to transfer data."
+  desc "HTTP/3 version curl."
   homepage "https://github.com/cntrump/build_curl/"
-  url "https://github.com/cntrump/homebrew-curlx/releases/download/v7.73.0/curlx-7.73.0_macosx.zip"
+  url "https://github.com/cntrump/homebrew-curlx/releases/download/v7.73.0/curlx-7.73.0-macos.zip"
   sha256 "96d3b2de00501375bd3e6468ce0bbd7ff5f235987f040227239ee9442489de6f"
 
   def install
