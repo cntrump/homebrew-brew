ecf03a2ad27cd118b5b394cea7aeb8b05d586608
cntrump <cntrump@gmail.com>
2020-12-06 16:35:33 +0800

    Create font-sfmono.rb

---
+++ Casks/font-sfmono.rb
@@ -0,0 +1,12 @@
+cask "font-sfmono" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/cntrump/sf-mono",
+      using:      :git
+  name "SF Mono"
+  homepage "https://developer.apple.com/fonts/"
+
+  font "SF-Mono-Italic.ttf"
+  font "SF-Mono.ttf"
+end
