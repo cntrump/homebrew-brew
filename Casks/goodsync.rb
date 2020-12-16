e1fcd5dad72d621ed1f0cc2f593acb9744348a3f
cntrump <cntrump@gmail.com>
2020-12-16 17:05:07 +0800

    Update goodsync.rb

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,10 +1,8 @@
 cask "goodsync" do
-  version "10.14.8.8"
-  sha256 "d8a896f29f78d6748dcf185b036f09da0d59c15cada3ce652c24fe56c6afae47"
+  version "10"
+  sha256 "no_check"
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
-  appcast "https://www.goodsync.com/download?os=macos",
-          must_contain: version.major_minor_patch
   name "GoodSync"
   desc "File synchronization and backup software"
   homepage "https://www.goodsync.com/"
